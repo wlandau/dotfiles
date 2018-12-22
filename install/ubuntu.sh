d2e74ad16ed90a388b7064484667bf5a363a65d7
wlandau <will.landau@gmail.com>
2018-12-22 16:34:18 -0500

    Add gdebi

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -2,5 +2,6 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y build-essential
 sudo apt-get install -y emacs
+sudo apt-get install -y gdebi
 sudo apt-get install -y git
 sudo apt-get install -y xclip

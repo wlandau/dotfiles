8c5b14239d1c5398e09e06e4e23b26cfacc982f6
wlandau <will.landau@gmail.com>
2018-12-22 15:47:21 -0500

    Work on installation scripts

---
+++ install/r-deps.sh
@@ -0,0 +1,17 @@
+agi fort77
+agi gfortran
+agi libbz2-dev
+agi libcurl4-openssl-dev
+agi liblzma-dev
+agi libpcre3
+agi libpcre3-dev
+agi libssl-dev
+agi libudunits2-dev
+agi libxml2-dev
+agi libzmq5
+agi libzmq5-dev
+agi texinfo
+agi texlive
+agi texlife-fonts-extra
+agi xorg-dev
+agi zlib1g-dev

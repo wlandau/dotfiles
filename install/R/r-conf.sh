7c52710abd6d37923820b217ce7f472e71800236
Will Landau <will.landau@gmail.com>
2024-08-09 16:01:03 -0400

    Update r-conf.sh

--- install/R/r-conf.sh
+++ install/R/r-conf.sh
@@ -1,3 +1,1 @@
-./configure --enable-R-shlib --with-cairo --enable-memory-profiling --without-recommended-packages
-sed -i -e 's/^[[1;35;40m^[[//g' src/include/Rconfig.h; rm src/include/Rconfig.h-e
-sed -i -e 's/^[[m^[[K//g' src/include/Rconfig.h; rm src/include/Rconfig.h-e
+./configure --enable-R-shlib --with-cairo --enable-memory-profiling

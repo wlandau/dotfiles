08e4bb6789718d79ec6d52ca40913af7ccee455b
Will Landau <1580860+wlandau@users.noreply.github.com>
2024-08-09 15:31:27 -0400

    Update r-conf.sh

--- install/R/r-conf.sh
+++ install/R/r-conf.sh
@@ -1,2 +1,1 @@
-# Just some notes about ./configure flags:
-./configure CFLAGS="-I/opt/R/arm64/include" CXXFLAGS="-I/opt/R/arm64/include" --enable-R-shlib --with-cairo --enable-memory-profiling
+./configure --enable-R-shlib --with-cairo --enable-memory-profiling --without-recommended-packages
